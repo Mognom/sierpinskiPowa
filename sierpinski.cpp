971962ad6e92e1a8e170af899e21816260903e3a
Mognom <alex.mognom@gmail.com>
2016-01-07 17:49:22 +0100

    Primer intento

--- sierpinski.cpp
+++ sierpinski.cpp
@@ -10,14 +10,34 @@ Sierpinski::Sierpinski(Triangulo t){
 
 
 void Sierpinski::calcularSierpinski(Triangulo t){
-	calcularSierpinski(t, 0);
+	listaTrabajos.push_back(t, 0);
+	
+	Triangulo triActual;
+	int stepActual;
+	
+	std::tuple<Triangulo,int> tuplaActual;
+	
+	//Calcula cuantos hay al final
+	stop = Math.pow(MAXSTEPS-1,3);
+	
+	#pragma omp parallel private(triActual, stepActual, tuplaActual) shared(listaResultado, listaTrabajos,stop)
+	while (stop > 0){
+		//Lee el trabajo actual
+		tuplaActual = listaTrabajos.pop();
+		triActual = std::get<0> (tuplaActual);
+		stepActual = std::get<1> (tuplaActual);
+		
+		//Realiza el trabajo
+		calcularSierpinski(triActual, stepActual);
+	}
 }
 
 void Sierpinski::calcularSierpinski(Triangulo t, int step){
 	
 	//Si es el ultimo paso, añade el triangulo que tiene a la lista final
 	if(step >= MAXSTEPS){
-		lista.push_back(t);
+		listaResultado.push_back(t);
+		stop--;
 		return;
 	}
 	
@@ -34,8 +54,12 @@ void Sierpinski::calcularSierpinski(Triangulo t, int step){
 	//Precalcula el step
 	step = step+1;
 	
+	listaTrabajos.push_back(std::tuple<Triangulo,int> (Triangulo(puntoA, aux1, aux3), step));
+	listaTrabajos.push_back(std::tuple<Triangulo,int> (Triangulo(aux1, puntoB, aux2), step));
+	listaTrabajos.push_back(std::tuple<Triangulo,int> (Triangulo(aux3, aux2, puntoC), step));
+	
 	//Calcula los siguientes pasos de forma recursiva
-    calcularSierpinski(Triangulo(puntoA, aux1, aux3), step);
-	calcularSierpinski(Triangulo(aux1, puntoB, aux2), step);
-    calcularSierpinski(Triangulo(aux3, aux2, puntoC), step);
+    //calcularSierpinski(Triangulo(puntoA, aux1, aux3), step);
+	//calcularSierpinski(Triangulo(aux1, puntoB, aux2), step);
+    //calcularSierpinski(Triangulo(aux3, aux2, puntoC), step);
 }
--- sierpinski.h
+++ sierpinski.h
@@ -4,12 +4,15 @@
 #define MAXSTEPS 13 //MAX 13
 
 #include <list>
+#include <tuple>
 #include "triangulo.h"
 
 class Sierpinski{
 
 private:
-    std::list<Triangulo> lista;
+    std::list<Triangulo> listaResultado;
+	std::list<std::tuple<Triangulo, int>> listaTrabajos;
+	int stop;
 
     void calcularSierpinski(Triangulo t);
     void calcularSierpinski(Triangulo t, int step);
